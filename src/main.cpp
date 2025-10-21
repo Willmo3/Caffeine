22bb75f977eff4110212dd6dc5b01e2bb0137db4
willmo3 <thelordofbouge@gmail.com>
2025-10-21 17:31:40 -0400

    Add file I/O sanity test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
-#include <ostream>
-
+#include <fstream>
+#include "cereal/archives/json.hpp"
 #include "WaffineForm.hpp"
 
 int main() {
+    // Quick I/O sanity test.
+    std::ofstream file("out.json");
+    cereal::JSONOutputArchive archive(file);
+
+    WaffineForm w = WaffineForm(Winterval(-2, 3));
+    w = w * WaffineForm(Winterval(4, 5)) + WaffineForm(Winterval(-1, 1));
+
+    archive(w);
     return 0;
 }
