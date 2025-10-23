5565044d02de7b96bf64f7dffd3062692e741fba
willmo3 <thelordofbouge@gmail.com>
2025-10-23 15:08:26 -0400

    Add waffine serialization stress test

--- test/test_waffine_serialization.cpp
+++ test/test_waffine_serialization.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "cereal/archives/json.hpp"
+#include "cereal/types/vector.hpp"
 #include "../src/WaffineForm.hpp"
 
 TEST(waffine_serialization, serialize_deserialize) {
@@ -16,15 +17,34 @@ TEST(waffine_serialization, serialize_deserialize) {
         oarchive(original);
     }
 
-    WaffineForm deserialized(Winterval(0,0));
+    auto deserialized = WaffineForm();
     {
         cereal::JSONInputArchive iarchive(ss);
         iarchive(deserialized);
     }
 
-    auto original_interval = original.to_interval();
-    auto deserialized_interval = deserialized.to_interval();
+    ASSERT_TRUE(original == deserialized);
+}
 
-    ASSERT_NEAR(original_interval.min(), deserialized_interval.min(), 0.001);
-    ASSERT_NEAR(original_interval.max(), deserialized_interval.max(), 0.001);
+TEST(waffine_serialization, serialize_vector) {
+    auto original = std::vector<WaffineForm>();
+
+    for (auto i = 0; i < 2000; i++) {
+        original.emplace_back(WaffineForm(Winterval(i, i + 1)));
+    }
+    std::stringstream ss;
+    {
+        cereal::JSONOutputArchive oarchive(ss);
+        oarchive(original);
+    }
+
+    auto new_vec = std::vector<WaffineForm>();
+    {
+        cereal::JSONInputArchive iarchive(ss);
+        iarchive(new_vec);
+    }
+
+    assert(original[0] == new_vec[0]);
+    assert(original[1] == new_vec[1]);
+    assert(original[1999] == new_vec[1999]);
 }
